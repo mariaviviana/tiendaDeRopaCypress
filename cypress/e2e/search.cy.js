646f571c2a3979b6b0155074383314ddad3d29fa
vivi <vivi@iMac-de-jeronimo.local>
2023-03-15 15:57:10 -0300

    Agregando Custom commands

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -5,20 +5,13 @@ describe('Search elements',() =>{
     })
 
     it('Search for elements with multiple results',()=>{
-        cy.fixture('index').then((index)=>{
-            cy.get(index.searchBox).type('iphone');
-            cy.get(index.searchButton).click();
-        })
+        cy.search('iphone');
         cy.fixture('searchResult').then((searchResult)=>{
             cy.get(searchResult.title).should('contain','iphone');
         })
     })
 
     it('Search for elements without results',()=>{
-        cy.fixture('index').then((index)=>{
-            cy.get(index.searchBox).type('autos');
-            cy.get(index.searchButton).click();
-        })
-
+        cy.search('pepe');
     })
 })
